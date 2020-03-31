46d68fc3c9a0ce643f3d418bd824ea89f7de8b38
Dmitry Zorin <dima.zor@mail.ru>
2020-03-31 18:05:52 +0300

    Remove dummy server

--- api/graphql/index.js
+++ api/graphql/index.js
@@ -2,8 +2,11 @@ require('dotenv').config({
 	path: __dirname + '/../.env.' + process.env.NODE_ENV,
 })
 const express = require('express')
-const { ApolloServer } = require('apollo-server-express')
+const session = require('express-session')
+const cors = require('cors')
+const MongoDBStore = require('connect-mongodb-session')(session)
 const passport = require('../_utilities/auth')
+const { ApolloServer } = require('apollo-server-express')
 const { buildContext } = require('graphql-passport')
 const typeDefs = require('../_utilities/typeDefs/all')
 const resolvers = require('../_utilities/resolvers/all')
@@ -11,8 +14,31 @@ const User = require('../_utilities/models/user.model')
 require('../_utilities/db')
 
 const app = express()
+const isProductionEnv = process.env.NODE_ENV === 'production'
+
+if (!isProductionEnv) {
+	app.use(cors({
+		origin: [process.env.UI_SERVER],
+		credentials: true,
+	}))
+}
+
+app.use(session({
+	secret: 'secret',
+	resave: true,
+	saveUninitialized: true,
+	store: new MongoDBStore({
+		uri: process.env.ATLAS_URI,
+		collection: 'sessions',
+	}),
+	cookie: {
+		maxAge: 1000 * 60 * 60 * 24 * 7,
+		secure: isProductionEnv,
+	},
+}))
 
 app.use(passport.initialize())
+app.use(passport.session())
 
 new ApolloServer({
 	typeDefs,
@@ -22,4 +48,8 @@ new ApolloServer({
 })
 	.applyMiddleware({ app, path: '/api/graphql', cors: false })
 
-app.listen(5000)
+const port = process.env.PORT || 5000
+
+app.listen(port, () =>
+	console.log(`Server is running on port: ${port}`),
+)
