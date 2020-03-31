2897038343e3793a9ecec295779d5058214a5016
Dmitry Zorin <dima.zor@mail.ru>
2020-03-31 17:05:33 +0300

    Update index.js

--- api/graphql/index.js
+++ api/graphql/index.js
@@ -1,23 +1,24 @@
+require('dotenv').config({
+	path: __dirname + '/../.env.' + process.env.NODE_ENV,
+})
 const express = require('express')
-const { ApolloServer, gql } = require('apollo-server-express')
+const { ApolloServer } = require('apollo-server-express')
+const passport = require('../_utilities/auth')
+const { buildContext } = require('graphql-passport')
+const typeDefs = require('../_utilities/typeDefs/all')
+const resolvers = require('../_utilities/resolvers/all')
+const User = require('../_utilities/models/user.model')
 
 const app = express()
 
+app.use(passport.initialize())
+
 new ApolloServer({
-	typeDefs: gql`
-    type Query {
-      user: Status!
-    }
-    type Status {
-      isAuthorized: Boolean
-    }
-	`,
-	resolvers: {
-		Query: {
-			user: () => ({ isAuthorized: false }),
-		},
-	},
+	typeDefs,
+	resolvers,
+	context: ({ req, res }) =>
+		buildContext({ req, res, User }),
 })
-.applyMiddleware({ app, path: '/api/graphql', cors: false })
+	.applyMiddleware({ app, path: '/api/graphql', cors: false })
 
 app.listen(5000)
