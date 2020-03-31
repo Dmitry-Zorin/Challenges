3a5ac72d6c432dc4958f8d77abe5a895e148e18d
Dmitry Zorin <dima.zor@mail.ru>
2020-03-31 14:17:08 +0300

    Update index.js

--- api/graphql/index.js
+++ api/graphql/index.js
@@ -2,6 +2,8 @@ const app = require('../_utilities/app')
 
 const port = process.env.PORT
 
-app.listen(port, () =>
+const server = app.listen(() =>
 	console.log(`Server is running on port: ${port}`),
 )
+
+module.exports = server
